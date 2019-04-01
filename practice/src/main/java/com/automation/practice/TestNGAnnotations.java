3fb65f60fac98d2c94577441618a69dd1ddba010
Pawan Kumar <pavan0575@gmail.com>
2019-04-01 09:35:25 +0530

    Adding first test and updated pom.xml

---
+++ practice/src/main/java/com/automation/practice/TestNGAnnotations.java
@@ -0,0 +1,11 @@
+package com.automation.practice;
+
+import org.testng.annotations.Test;
+
+public class TestNGAnnotations {
+
+	@Test
+	public void testMethod() {
+		System.out.println("First test");
+	}
+}
